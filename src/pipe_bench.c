d52cb983b54308f18ac0424dfbf27b5057c34e8e
msdx321 <msdx321@gmail.com>
2020-08-14 17:55:00 +0800

    pipe_bench: Optimize bench logic
    * Now we measure both 1 way and roundtrip in one round.
    
    Signed-off-by: msdx321 <msdx321@gmail.com>

--- src/pipe_bench.c
+++ src/pipe_bench.c
@@ -12,70 +12,40 @@
 
 pthread_t t1, t2;
 int       pp[2], pp2[2], pp3[2], pp4[2];
-cycle_t * results;
+cycle_t * results, *results2, *results3;
 
 void
-reader_work_oneway()
+reader_work()
 {
-	cycle_t t, tt;
+	cycle_t ts;
+
+	CHECK(read(pp3[0], &ts, sizeof(cycle_t)));
+	CHECK(write(pp4[1], &ts, sizeof(cycle_t)));
 
 	for (int i = 0; i < ITERATION; i++) {
-		CHECK(write(pp4[1], &t, sizeof(cycle_t)));
-		CHECK(read(pp3[0], &t, sizeof(cycle_t)));
-
-		CHECK(read(pp[0], &t, sizeof(cycle_t)));
-		tt = get_cyclecount();
-		if (tt < t) { continue; }
-
-		results[i] = tt - t;
+		CHECK(read(pp[0], &ts, sizeof(cycle_t)));
+		ts = get_cyclecount();
+		CHECK(write(pp2[1], &ts, sizeof(cycle_t)));
 	}
 }
 
 void
-writer_work_oneway()
+writer_work()
 {
-	cycle_t t;
+	cycle_t ts, ts2, ts3;
+
+	CHECK(write(pp3[1], &ts, sizeof(cycle_t)));
+	CHECK(read(pp4[0], &ts, sizeof(cycle_t)));
 
 	for (int i = 0; i < ITERATION; i++) {
-		CHECK(read(pp4[0], &t, sizeof(cycle_t)));
-		CHECK(write(pp3[1], &t, sizeof(cycle_t)));
+		ts = get_cyclecount();
+		CHECK(write(pp[1], &ts, sizeof(cycle_t)));
+		CHECK(read(pp2[0], &ts2, sizeof(cycle_t)));
+		ts3 = get_cyclecount();
 
-		t = get_cyclecount();
-		CHECK(write(pp[1], &t, sizeof(cycle_t)));
-	}
-}
-
-void
-reader_work_roundtrip()
-{
-	cycle_t t;
-
-	CHECK(read(pp3[0], &t, sizeof(t)));
-	CHECK(write(pp4[1], &t, sizeof(t)));
-
-	for (int i = 0; i < ITERATION; i++) {
-		CHECK(read(pp[0], &t, sizeof(cycle_t)));
-		CHECK(write(pp2[1], &t, sizeof(cycle_t)));
-	}
-}
-
-void
-writer_work_roundtrip()
-{
-	cycle_t t, tt;
-
-	CHECK(write(pp3[1], &t, sizeof(cycle_t)));
-	CHECK(read(pp4[0], &t, sizeof(cycle_t)));
-
-	for (int i = 0; i < ITERATION; i++) {
-		t = get_cyclecount();
-		CHECK(write(pp[1], &t, sizeof(cycle_t)));
-		CHECK(read(pp2[0], &t, sizeof(cycle_t)));
-
-		tt = get_cyclecount();
-		if (tt < t) { continue; }
-
-		results[i] = tt - t;
+		results[i]  = ts2 - ts;
+		results2[i] = ts3 - ts2;
+		results3[i] = ts3 - ts;
 	}
 }
 
@@ -85,7 +55,7 @@ reader_thread(void *p)
 {
 	pthread_prio(SCHED_RR, t1, *(int *)p);
 
-	reader_work_oneway();
+	reader_work();
 
 	return NULL;
 }
@@ -95,27 +65,7 @@ writer_thread(void *p)
 {
 	pthread_prio(SCHED_RR, t2, *(int *)p);
 
-	writer_work_oneway();
-
-	return NULL;
-}
-
-void *
-reader2_thread(void *p)
-{
-	pthread_prio(SCHED_RR, t1, *(int *)p);
-
-	reader_work_roundtrip();
-
-	return NULL;
-}
-
-void *
-writer2_thread(void *p)
-{
-	pthread_prio(SCHED_RR, t2, *(int *)p);
-
-	writer_work_roundtrip();
+	writer_work();
 
 	return NULL;
 }
@@ -126,18 +76,20 @@ oneway_bench_processes()
 	pid_t p;
 	int   status;
 
-	printf("pipe + between processes + 1 way + reader w/ higher prio:\n");
+	printf("pipe + between processes:\n");
 	utils_clean_results(results);
+	utils_clean_results(results2);
+	utils_clean_results(results3);
 
 	p = fork();
 	if (p == 0) {
-		set_prio(SCHED_RR, 1);
-		writer_work_oneway();
+		set_prio(SCHED_RR, 0);
+		reader_work();
 
 		exit(EXIT_SUCCESS);
 	} else {
-		set_prio(SCHED_RR, 0);
-		reader_work_oneway();
+		set_prio(SCHED_RR, 1);
+		writer_work();
 	}
 
 	waitpid(p, &status, 0);
@@ -147,47 +99,17 @@ oneway_bench_processes()
 	utils_print_results(results);
 	utils_store_results(results, "pipe_bench-p_1w_rh.csv");
 
-	printf("pipe + between processes + 1 way + writer w/ higher prio:\n");
-	utils_clean_results(results);
+	utils_store_results(results2, "pipe_bench-p_1w_wh-unsorted.csv");
+	qsort(results2, ITERATION, sizeof(cycle_t), utils_compare);
+	utils_eliminate_zero(results2);
+	utils_print_results(results2);
+	utils_store_results(results2, "pipe_bench-p_1w_wh.csv");
 
-	p = fork();
-	if (p == 0) {
-		set_prio(SCHED_RR, 0);
-		writer_work_oneway();
-
-		exit(EXIT_SUCCESS);
-	} else {
-		set_prio(SCHED_RR, 1);
-		reader_work_oneway();
-	}
-
-	waitpid(p, &status, 0);
-	utils_store_results(results, "pipe_bench-p_1w_wh-unsorted.csv");
-	qsort(results, ITERATION, sizeof(cycle_t), utils_compare);
-	utils_eliminate_zero(results);
-	utils_print_results(results);
-	utils_store_results(results, "pipe_bench-p_1w_wh.csv");
-
-	printf("pipe + between processes + round trip:\n");
-	utils_clean_results(results);
-
-	p = fork();
-	if (p == 0) {
-		set_prio(SCHED_RR, 0);
-		reader_work_roundtrip();
-
-		exit(EXIT_SUCCESS);
-	} else {
-		set_prio(SCHED_RR, 1);
-		writer_work_roundtrip();
-	}
-
-	waitpid(p, &status, 0);
-	utils_store_results(results, "pipe_bench-p_rt-unsorted.csv");
-	qsort(results, ITERATION, sizeof(cycle_t), utils_compare);
-	utils_eliminate_zero(results);
-	utils_print_results(results);
-	utils_store_results(results, "pipe_bench-p_rt.csv");
+	utils_store_results(results3, "pipe_bench-p_rt-unsorted.csv");
+	qsort(results3, ITERATION, sizeof(cycle_t), utils_compare);
+	utils_eliminate_zero(results3);
+	utils_print_results(results3);
+	utils_store_results(results3, "pipe_bench-p_rt.csv");
 }
 
 void
@@ -195,8 +117,10 @@ oneway_bench_threads()
 {
 	int n1, n2;
 
-	printf("pipe + between threads + 1 way + reader w/ higher prio:\n");
+	printf("pipe + between threads:\n");
 	utils_clean_results(results);
+	utils_clean_results(results2);
+	utils_clean_results(results3);
 
 	n1 = 0;
 	n2 = 1;
@@ -211,37 +135,17 @@ oneway_bench_threads()
 	utils_print_results(results);
 	utils_store_results(results, "pipe_bench-t_1w_rh.csv");
 
-	printf("pipe + between threads + 1 way + writer w/ higher prio:\n");
-	utils_clean_results(results);
+	utils_store_results(results2, "pipe_bench-t_1w_wh-unsorted.csv");
+	qsort(results2, ITERATION, sizeof(cycle_t), utils_compare);
+	utils_eliminate_zero(results2);
+	utils_print_results(results2);
+	utils_store_results(results2, "pipe_bench-t_1w_wh.csv");
 
-	n1 = 1;
-	n2 = 0;
-	pthread_create(&t1, NULL, reader_thread, &n1);
-	pthread_create(&t2, NULL, writer_thread, &n2);
-	pthread_join(t1, NULL);
-	pthread_join(t2, NULL);
-
-	utils_store_results(results, "pipe_bench-t_1w_wh-unsorted.csv");
-	qsort(results, ITERATION, sizeof(cycle_t), utils_compare);
-	utils_eliminate_zero(results);
-	utils_print_results(results);
-	utils_store_results(results, "pipe_bench-t_1w_wh.csv");
-
-	printf("pipe + between threads + round trip:\n");
-	utils_clean_results(results);
-
-	n1 = 0;
-	n2 = 1;
-	pthread_create(&t1, NULL, reader2_thread, &n1);
-	pthread_create(&t2, NULL, writer2_thread, &n2);
-	pthread_join(t1, NULL);
-	pthread_join(t2, NULL);
-
-	utils_store_results(results, "pipe_bench-t_rt-unsorted.csv");
-	qsort(results, ITERATION, sizeof(cycle_t), utils_compare);
-	utils_eliminate_zero(results);
-	utils_print_results(results);
-	utils_store_results(results, "pipe_bench-t_rt.csv");
+	utils_store_results(results3, "pipe_bench-t_rt-unsorted.csv");
+	qsort(results3, ITERATION, sizeof(cycle_t), utils_compare);
+	utils_eliminate_zero(results3);
+	utils_print_results(results3);
+	utils_store_results(results3, "pipe_bench-t_rt.csv");
 }
 
 int
@@ -255,7 +159,9 @@ main(int argc, char *argv[])
 	pipe(pp3);
 	pipe(pp4);
 
-	results = malloc(sizeof(cycle_t) * ITERATION);
+	results  = malloc(sizeof(cycle_t) * ITERATION);
+	results2 = malloc(sizeof(cycle_t) * ITERATION);
+	results3 = malloc(sizeof(cycle_t) * ITERATION);
 
 	init_perfcounters(1, 0);
 
@@ -272,6 +178,8 @@ main(int argc, char *argv[])
 	close(pp4[1]);
 
 	free(results);
+	free(results2);
+	free(results3);
 
 	return 0;
 }
